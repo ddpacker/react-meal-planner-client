7cbdaba4e2d78984b93252201571aa9be08b372f
Darren Packer <darren.d.packer@gmail.com>
2026-06-03 15:10:48 -0400

    test: fix test linting

--- vitest.setup.ts
+++ vitest.setup.ts
@@ -1,6 +1,7 @@
-import '@testing-library/jest-dom';
+import '@testing-library/jest-dom/vitest';
+import { afterAll, afterEach, beforeAll } from 'vitest';
 import { server } from './src/__mocks__/server';
 
-beforeAll(() => server.listen());
+beforeAll(() => server.listen({ onUnhandledRequest: 'bypass' }));
 afterEach(() => server.resetHandlers());
 afterAll(() => server.close());
